bd6bf36bc28fafb5b1b2e0290d0ed93cfe2355cf
Chandra Chinnaiah <kshobhz@gmail.com>
2024-07-01 22:19:02 +0100

    Adding e-learning tests bdd tests

---
+++ resources/config.py
@@ -0,0 +1,8 @@
+class Config:
+
+    BROWSER="Chrome"
+    # URLs
+    BASE_URL = "https://learning.elucidat.com/course/5c9126fd760e5-611a53751213a"
+    # Credentials
+    USERNAME = ""
+    PASSWORD = ""
